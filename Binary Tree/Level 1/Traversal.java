79239c3699cbd818fefc0cc6592d5232728d81e2
Varis Rana <vrvarisrana123@gmail.com>
2021-12-30 06:37:05 +0530

    commit 30

--- Binary Tree/Level 1/Traversal.java
+++ Binary Tree/Level 1/Traversal.java
@@ -4,7 +4,16 @@ class Node {
       int data;
       Node left;
       Node right;
-      
+
+      Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+      }
+
+      Node(int data) {
+            this(data, null, null);
+      }
 }
 
 public class Traversal {
