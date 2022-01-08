a527242e5c2639dac64bec56b88f084137c2415f
Varis Rana <vrvarisrana123@gmail.com>
2022-01-08 18:40:16 +0530

    commit 08

--- Binary Tree/Level 1/countLeaves.java
+++ Binary Tree/Level 1/countLeaves.java
@@ -1,4 +1,11 @@
 import java.util.*;
+
 public class countLeaves {
-      
+      public static int cL(Node root) {
+            if (root == null)
+                  return 0;
+            if (root.left == null && root.right == null)
+                  return 1;
+            return cL(root.left) + cL(root.right);
+      }
 }
