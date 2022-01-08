38dfa03471c779ada7816a1b0ca541a8dff33d54
Varis Rana <vrvarisrana123@gmail.com>
2022-01-08 17:46:59 +0530

    commit 08

--- Binary Tree/basicFunctions.java
+++ Binary Tree/basicFunctions.java
@@ -1,5 +1,22 @@
 import java.util.*;
 
 public class basicFunctions {
+      // size
+      public static void size(Node root) {
 
+      }
+
+      // sum
+      public static int sum(Node root) {
+
+      }
+
+      // Max,Min
+      public static int max(Node root) {
+
+      }
+
+      public static int min(Node root) {
+
+      }
 }
