2daf13306d39e7b57348d8274b70833ac953edac
Varis Rana <vrvarisrana123@gmail.com>
2022-01-09 11:42:21 +0530

    commit 09

--- Level1/Binary Tree/isBalancedTree.java
+++ Level1/Binary Tree/isBalancedTree.java
@@ -4,4 +4,17 @@ public class isBalancedTree {
       public static int height(Node root) {
             return root == null ? -1 : Math.max(height(root.left), height(root.right)) + 1;
       }
+
+      public static boolean isBal(Node root) {
+            if(root==null)
+            return true;
+            if(!isBal(root.left)) return false;
+            if(!isBal(root.right)) return true;
+            int lh=height(root.left);
+            int rh=height(root.right);
+            int diff=Math.abs(lh-rh);
+            if(diff>1)
+            return false;
+            return true;
+      }
 }
