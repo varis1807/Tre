b241eba9d0450274b401a6d5c0ed656b64e4fabb
Varis Rana <vrvarisrana123@gmail.com>
2022-01-09 10:22:21 +0530

    commit 09

--- Level1/Binary Search Tree/checkBST.java
+++ Level1/Binary Search Tree/checkBST.java
@@ -16,8 +16,20 @@ class Node {
 }
 
 public class checkBST {
-      static Node prev=null;
-      public static boolean isBST(Node root) {
+      static Node prev = null;
 
+      public static boolean isBST(Node root) {
+            if (root == null)
+                  return true;
+            if (!isBST(root.left))
+                  return false;
+
+            if (prev != null && prev.data >= root.data)
+                  return false;
+            prev = root;
+            if (!isBST(root.right))
+                  return false;
+
+            return true;
       }
 }
