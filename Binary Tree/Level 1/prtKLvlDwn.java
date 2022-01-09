405dadec231d0c827e062855db95be4b99f35c18
Varis Rana <vrvarisrana123@gmail.com>
2022-01-09 06:54:53 +0530

    commit 09

--- Binary Tree/Level 1/prtKLvlDwn.java
+++ Binary Tree/Level 1/prtKLvlDwn.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class prtKLvlDwn {
       public static void kLevelDwn(Node root, int k, ArrayList<Integer> ans) {
-
+            if (root == null || k < 0)
+                  return;
+            if (k == 0) {
+                  ans.add(root.data);
+                  return;
+            }
+            kLevelDwn(root.left, k-1, ans);
+            kLevelDwn(root.right, k-1, ans);
       }
 }
