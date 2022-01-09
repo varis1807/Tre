048b5b7a0ed10f983ca95d5dc43f8766359f290f
Varis Rana <vrvarisrana123@gmail.com>
2022-01-09 09:40:13 +0530

    commit 09

---
+++ Binary Tree/Level 1/prtKDistAway.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class prtKDistAway {
+      public static boolean nTrP(Node root, int data, ArrayList<Node> ans) {
+            if (root == null)
+                  return false;
+            if (root.data == data)
+                  ans.add(root);
+            boolean res = nTrP(root.left, data, ans) || nTrP(root.right, data, ans);
+            if (res)
+                  ans.add(root);
+            return res;
+      }
+
+      public static void kLevelDwn(Node root, int k, Node block, ArrayList<Node> ans) {
+            if (root == null || k < 0)
+                  return;
+            if (k == 0) {
+                  ans.add(root);
+                  return;
+            }
+            kLevelDwn(root.left, k - 1, block, ans);
+            kLevelDwn(root.right, k - 1, block, ans);
+      }
+
+      public static ArrayList<Node> kLvlDwn(Node root, int data, int k) {
+            ArrayList<Node> list = new ArrayList<>();
+            nTrP(root, data, list);
+            ArrayList<Node> ans = new ArrayList<>();
+            Node block = null;
+            for (int i = 0; i < list.size(); i++) {
+                  kLevelDwn(list.get(i), k - i, block, ans);
+                  block = list.get(i);
+            }
+            return ans;
+      }
+}
