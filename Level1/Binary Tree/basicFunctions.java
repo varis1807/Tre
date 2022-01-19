971225e96de5a0867b9d1df7fcd738fdbb6d927b
Varis Rana <vrvarisrana123@gmail.com>
2022-01-19 09:47:34 +0530

    commit 19

--- Level1/Binary Tree/basicFunctions.java
+++ Level1/Binary Tree/basicFunctions.java
@@ -113,4 +113,17 @@ public class basicFunctions {
 
             return find(root.left, data) || find(root.right, data);
       }
+
+      public static void main(String[] args) {
+            Node root = new Node(2);
+            root.left = new Node(1);
+            root.right = new Node(3);
+            root.left.left = new Node(0);
+            root.left.right = new Node(5); 
+            root.right.left=new Node(4);
+            root.right.right=new Node(9);
+            root.right.right.left=new Node(7);
+            root.right.right.right=new Node(8);
+            Sysi
+      }
 }
