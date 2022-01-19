5a7bb5ecc428f6a9030514055134a1c0bdf0c3b4
Varis Rana <vrvarisrana123@gmail.com>
2022-01-19 15:16:57 +0530

    commit 19

--- Level1/Binary Search Tree/print_in_range.java
+++ Level1/Binary Search Tree/print_in_range.java
@@ -6,12 +6,21 @@ public class print_in_range {
         printInRange(node.left, lr, rr);
 
         if (node.data >= lr && node.data <= rr)
-            System.out.print(node.data);
+            System.out.println(node.data);
 
         printInRange(node.right, lr, rr);
     }
 
     public static void main(String[] args) {
-
+        Node root = new Node(10);
+        root.left = new Node(7);
+        root.right = new Node(11);
+        root.left.left = new Node(5);
+        root.left.right = new Node(8);
+        root.left.right.left = new Node(6);
+        root.right.left = new Node(9);
+        root.right.right = new Node(13);
+        root.right.right.right = new Node(15);
+        printInRange(root, 5, 15);
     }
 }
