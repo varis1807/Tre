db42f009f241aa3e6805cba39f87af7441e7def0
Varis Rana <vrvarisrana123@gmail.com>
2022-01-19 09:13:47 +0530

    commit 19

--- Level1/Binary Tree/basicFunctions.java
+++ Level1/Binary Tree/basicFunctions.java
@@ -1,5 +1,19 @@
 import java.util.*;
+class Node {
+      int data;
+      Node left;
+      Node right;
 
+      Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+      }
+
+      Node(int data) {
+            this(data, null, null);
+      }
+}
 public class basicFunctions {
       // size
       public static int size(Node root) {
