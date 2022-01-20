80db417cc33c3e9fcbea35a0c295a29df67ca1d9
Varis Rana <vrvarisrana123@gmail.com>
2022-01-20 09:26:55 +0530

    commit 20

--- Level1/Binary Search Tree/basicFunction.java
+++ Level1/Binary Search Tree/basicFunction.java
@@ -9,7 +9,7 @@ public class basicFunction {
 
       // Height-----------------------------
       public static int height(Node root) {
-            return root == null ? -1 : Math.max(height(root.left) + height(root.right)) + 1;
+            return root == null ? -1 : Math.max(height(root.left), height(root.right)) + 1;
       }
 
       // sum------------------------------------
