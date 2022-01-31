6240b079bd50e3be3b0c80224fbaed1af725e68b
Varis Rana <vrvarisrana123@gmail.com>
2022-01-31 19:38:25 +0530

    commit 31

--- Que/CousinsOfGivenNode.java
+++ Que/CousinsOfGivenNode.java
@@ -1,4 +1,11 @@
 import java.util.*;
 public class CousinsOfGivenNode{
-
+      public static ArrayList<Integer> printCousins(Node root, Node node_to_find)
+      {
+          //code here
+          if(root==null)
+          return ArrayList<Integer>{-1};
+          
+          
+      }
 }
