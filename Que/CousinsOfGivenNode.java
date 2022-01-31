6231281dd9b4cfa166f38bbb0be2fb8298aaf8c6
Varis Rana <vrvarisrana123@gmail.com>
2022-01-31 19:39:52 +0530

    commit 31

--- Que/CousinsOfGivenNode.java
+++ Que/CousinsOfGivenNode.java
@@ -1,15 +1,18 @@
 import java.util.*;
-public class CousinsOfGivenNode{
+
+public class CousinsOfGivenNode {
       static class Node {
-      int data;
-      Node left;
-      Node right;
-      Node(int data) {
-          this.data = data;
-          left = null;
-          right = null;
+            int data;
+            Node left;
+            Node right;
+
+            Node(int data) {
+                  this.data = data;
+                  left = null;
+                  right = null;
+            }
       }
-  } 
+
       public static ArrayList<Integer> printCousins(Node root, Node node_to_find)
       {
           //code here
