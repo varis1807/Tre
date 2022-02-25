a1cc67b71ce1538b0f2da15a9742277806ca9337
Varis Rana <vrvarisrana123@gmail.com>
2022-02-25 14:53:28 +0530

    commit 25

--- Construction Que/BTFromPreInOrderTraversal.java
+++ Construction Que/BTFromPreInOrderTraversal.java
@@ -1,4 +1,56 @@
 import java.util.*;
-public class BTFromPreInOrderTraversal{
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+
+      TreeNode() {
+      }
+
+      TreeNode(int val) {
+            this.val = val;
+      }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+      }
+}
+
+public class BTFromPreInOrderTraversal {
+      // leet 105 =======================================================
+
+      public TreeNode construct(int[] pre, int psi, int pei, int[] in, int isi, int iei) {
+            if (psi == pei) {
+                  return new TreeNode(pre[psi]);
+            }
+
+            if (psi > pei) {
+                  return null;
+            }
+
+            TreeNode root = new TreeNode(pre[psi]);
+
+            int find_ele = pre[psi];
+
+            int idx = isi;
+
+            while (in[idx] != find_ele) {
+                  idx++;
+            }
+
+            int left_ele = idx - isi;
+
+            root.left = construct(pre, psi + 1, psi + left_ele, in, isi, idx - 1);
+            root.right = construct(pre, psi + left_ele + 1, pei, in, idx + 1, iei);
+
+            return root;
+      }
+
+      public TreeNode buildTree(int[] preorder, int[] inorder) {
+            return construct(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+      }
 
 }
