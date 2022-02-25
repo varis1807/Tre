3022ee486ff46bef1bd6856a43a70205c97adbe2
Varis Rana <vrvarisrana123@gmail.com>
2022-02-25 14:58:43 +0530

    commit 25

--- Construction Que/BTFromIN_POSTorderTraversal.java
+++ Construction Que/BTFromIN_POSTorderTraversal.java
@@ -1,4 +1,29 @@
 import java.util.*;
 public class BTFromIN_POSTorderTraversal {
-      
+  // leet 106 ============================================== 
+
+  public TreeNode construct2(int[] post, int psi, int pei, int[] in, int isi, int iei){
+      if(psi>pei) return null;
+
+      TreeNode root=new TreeNode(post[pei]);
+
+
+      int idx=isi;
+      int find_ele=post[pei];
+
+      while(in[idx]!=find_ele){
+          idx++;
+      }        
+
+      int left_ele=idx-isi;
+
+      root.left=construct2(post,psi,psi+left_ele-1,in,isi,idx-1);
+      root.right=construct2(post,psi+left_ele,pei-1,in,idx+1,iei);
+
+      return root;
+  }
+
+  public TreeNode buildTree(int[] inorder, int[] postorder) {
+      return construct2(postorder,0,postorder.length-1,inorder,0,inorder.length-1);
+  }    
 }
